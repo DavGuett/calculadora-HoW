d9320fc359a6e055725bbe2a3793b52a70a05c36
DavGuetten <soulsec@protonmail.com>
2022-03-28 16:21:47 -0300

    Commit inicial do projeto

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int opcao;
+    cout << "Bem vindo! Escolha uma opção de conversão: " << endl;
+    cout << "Opção 1 - Conversão Decimal para Binário" << endl;
+    cout << "Opção 2 - Conversão Binário para Decimal" << endl;
+    cin >> opcao;
+    cout << "O valor digitado foi" << opcao;
+    return 0;
+}

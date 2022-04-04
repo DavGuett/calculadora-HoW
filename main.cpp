6afa39b7d3aa5f3def08db55768dcb87ffcfa2f7
DavGuetten <soulsec@protonmail.com>
2022-04-04 17:21:36 -0300

    Criada a estrutura condicional básica

--- main.cpp
+++ main.cpp
@@ -5,10 +5,21 @@ using namespace std;
 int main()
 {
     int opcao;
+    int numero;
     cout << "Bem vindo! Escolha uma opção de conversão: " << endl;
     cout << "Opção 1 - Conversão Decimal para Binário" << endl;
     cout << "Opção 2 - Conversão Binário para Decimal" << endl;
     cin >> opcao;
-    cout << "O valor digitado foi" << opcao;
+    if (opcao == 1) {
+        cout << "Você escolheu fazer a conversão de decimal para binário" << endl;
+        cout << "Digite o número que deseja converter: " << endl;
+        cin >> numero;
+    } else if (opcao == 2) {
+        cout << "Você escolheu fazer a conversão de binário para decimal" << endl;
+        cout << "Digite o número que deseja converter: " << endl;
+        cin >> numero;
+    } else {
+        cout << "Favor escolher entre as opções 1 e 2" << endl;
+    }
     return 0;
 }

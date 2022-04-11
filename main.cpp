3c67fed2b589ce94cf82cef7a6ff2de5e8b7ad7c
DavGuetten <soulsec@protonmail.com>
2022-04-10 21:19:33 -0300

    Adicionado gitignore e refatorado o código

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
+// Calculadora de Conversão Binária HoW I
+// David Jeziel de Almeida Guetten.
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+    bool executar = true;
     int opcao;
     int numero;
+    while (executar) {
     cout << "Bem vindo! Escolha uma opção de conversão: " << endl;
     cout << "Opção 1 - Conversão Decimal para Binário" << endl;
     cout << "Opção 2 - Conversão Binário para Decimal" << endl;
+    cout << "Opção 3 - Sair" << endl;
     cin >> opcao;
     if (opcao == 1) {
         cout << "Você escolheu fazer a conversão de decimal para binário" << endl;
@@ -18,8 +24,11 @@ int main()
         cout << "Você escolheu fazer a conversão de binário para decimal" << endl;
         cout << "Digite o número que deseja converter: " << endl;
         cin >> numero;
-    } else {
-        cout << "Favor escolher entre as opções 1 e 2" << endl;
+    } else if (opcao == 3) {
+        cout << "Você escolheu sair..." << endl;
+        executar = false;
     }
+    }
+
     return 0;
 }

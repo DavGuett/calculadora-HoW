bc64c75ef1c8a0b051ee4b5226fc8f0f6d75b651
DavGuetten <soulsec@protonmail.com>
2022-04-15 10:17:49 -0300

    Primeira etapa finalizada

--- main.cpp
+++ main.cpp
@@ -9,26 +9,44 @@ int main()
 {
     bool executar = true;
     int opcao;
-    int numero;
+    int numeroDecimal;
     while (executar) {
-    cout << "Bem vindo! Escolha uma opção de conversão: " << endl;
-    cout << "Opção 1 - Conversão Decimal para Binário" << endl;
-    cout << "Opção 2 - Conversão Binário para Decimal" << endl;
-    cout << "Opção 3 - Sair" << endl;
+    cout << "Bem vindo! Escolha uma opcao de conversao: " << endl;
+    cout << "Opcao 1 - Conversao Decimal para Binario" << endl;
+    cout << "Opcao 2 - Conversao Binario para Decimal" << endl;
+    cout << "Opcao 3 - Sair" << endl;
     cin >> opcao;
     if (opcao == 1) {
-        cout << "Você escolheu fazer a conversão de decimal para binário" << endl;
-        cout << "Digite o número que deseja converter: " << endl;
-        cin >> numero;
+        int numeroBinario[8];
+        cout << "Voce escolheu fazer a conversao de decimal para binario" << endl;
+        cout << "Digite o numero que deseja converter: " << endl;
+        cin >> numeroDecimal;
+        for(int i = 7; i >= 0; i--) {
+            if(numeroDecimal % 2 == 0){
+                numeroBinario[i] = 0;
+            } else {
+                numeroBinario[i] = 1;
+            }
+            numeroDecimal = numeroDecimal / 2;
+        }
+        cout << "O numero em binario e: ";
+        for(int i = 0; i < 8; i++) {
+            cout << numeroBinario[i];
+        }
+        cout << endl;
     } else if (opcao == 2) {
-        cout << "Você escolheu fazer a conversão de binário para decimal" << endl;
-        cout << "Digite o número que deseja converter: " << endl;
-        cin >> numero;
+        int numeroBinario;
+        cout << "Voce escolheu fazer a conversao de binario para decimal" << endl;
+        cout << "Digite o numero que deseja converter: " << endl;
+        cin >> numeroBinario;
     } else if (opcao == 3) {
-        cout << "Você escolheu sair..." << endl;
+        cout << "Voce escolheu sair..." << endl;
         executar = false;
+        exit;
+    }
+     else {
+        cout << "Favor escolher uma opcao valida" << endl;
     }
     }
-
     return 0;
 }

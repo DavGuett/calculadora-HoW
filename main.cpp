7b2482eb610c4109e5af5fdd6c523ff196237314
DavGuetten <soulsec@protonmail.com>
2022-05-12 21:04:48 -0300

    Lógica de conversão binário-decimal

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 // David Jeziel de Almeida Guetten.
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -9,7 +10,8 @@ int main()
 {
     bool executar = true;
     int opcao;
-    int numeroDecimal;
+    int numeroDecimal = 0;
+    int numeroBinario = 0;
     while (executar) {
     cout << "Bem vindo! Escolha uma opcao de conversao: " << endl;
     cout << "Opcao 1 - Conversao Decimal para Binario" << endl;
@@ -35,10 +37,15 @@ int main()
         }
         cout << endl;
     } else if (opcao == 2) {
-        int numeroBinario;
         cout << "Voce escolheu fazer a conversao de binario para decimal" << endl;
         cout << "Digite o numero que deseja converter: " << endl;
         cin >> numeroBinario;
+        for (int i = 0; numeroBinario > 0; i++) {
+            numeroDecimal = numeroDecimal + pow(2, i) * (numeroBinario % 10);
+            numeroBinario = numeroBinario / 10;
+        }
+        cout << numeroDecimal << endl;
+        cout << "O numero em decimal e: " << numeroDecimal << endl;
     } else if (opcao == 3) {
         cout << "Voce escolheu sair..." << endl;
         executar = false;
